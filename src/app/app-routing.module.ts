f2543d79e599b900921157938dc7551c6e09534d
halterdev <halterdev@gmail.com>
2018-06-11 22:01:38 -0400

    auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardService as AuthGuard } from './auth/auth-guard.service';
 
 import { LoginRegisterComponent } from './components/loginRegister/loginRegister.component';
 
